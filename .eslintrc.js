a3bc27a61e8b84fb5fb5e1c62a78b5b4613447e4
T Rahul Thampi <trahulthampi@gmail.com>
2018-01-23 02:37:05 +0530

    lint rule changes

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "env": {
+    "mocha": true
+  },
+  "rules": {
+    "no-tabs": 0,
+    "indent": ["error", "tab"],
+    "quotes": ["error", 'single'],
+    "jsx-quotes": ["error", "prefer-single"],
+    "react/jsx-indent": ["error", 'tab'],
+  },
+};
