e3a1f5cead979dd009a53d4333071c0b0640dedc
prasanna1211 <prasanna@codebrahma.com>
2018-02-04 00:39:38 +0530

    WIP: added POST error handler for registration

--- server/registration/registrationHandler.js
+++ server/registration/registrationHandler.js
@@ -1,17 +1,45 @@
 const boom = require('boom');
+const UserModel = require('./registrationSchema');
 
+/*
+ * Register Function registers with userName, email, password
+ */
 const register = async function ({
-	query: {
+	payload: {
 		userName,
+		email,
+		password,
 	},
-}, res) {
+}) {
 	try {
-		return {
-			name: userName,
-		};
+		// Create a new instance of UserModel
+		const currentUser = new UserModel({
+			userName,
+			email,
+			password,
+		});
+
+		// Save the currentUser
+		currentUser
+			.save()
+			.then(() => currentUser)
+			.catch((error) => {
+				const errorMessage = 'Data Base error';
+				return boom.boomify(error, {
+					statusCode: 500,
+					message: errorMessage,
+				});
+			});
+
+		// Return the currentUser
+		return currentUser;
 	} catch (error) {
+		// Return if error
 		const errorMessage = 'API Failed';
-		return res(boom.boomify(error, { statusCode: 500, message: errorMessage }));
+		return boom.boomify(error, {
+			statusCode: 500,
+			message: errorMessage,
+		});
 	}
 };
 
