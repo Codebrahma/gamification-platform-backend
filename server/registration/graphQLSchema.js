3cb393f9c8fd38d13e494adaf299fcc291b6b308
prasanna1211 <prasanna@codebrahma.com>
2018-02-04 13:42:52 +0530

    Changed schema for creating user

--- server/registration/graphQLSchema.js
+++ server/registration/graphQLSchema.js
@@ -1,15 +1,16 @@
-
+// Schema for GraphQL
 const schema = `
 type User {
   id: ID!
   email: String!
+  userName: String!
 }
 type Query {
-  getUserById(id: ID!): User
   getUserByEmail(email: String!): User
+  getUserByUserName(userName: String!): User
 }
 type Mutation {
-  createUser(email: String!): User
+  createUser(email: String!, userName: String!, password: String!): User
 }
 schema {
   query: Query
