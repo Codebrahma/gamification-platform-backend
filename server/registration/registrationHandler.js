2e9162b717ff09c307898eab7dace76cdd3368c7
prasanna1211 <prasanna@codebrahma.com>
2018-02-04 16:20:12 +0530

    refactor(user-registration): changed REST for create User
    Change for create user code

--- server/registration/registrationHandler.js
+++ server/registration/registrationHandler.js
@@ -1,5 +1,7 @@
 const boom = require('boom');
-const UserModel = require('./registrationSchema');
+const UserModel = require('../../db/schema/userSchema');
+
+const createUser = require('../common/createUser');
 
 /*
  * Register Function registers with userName, email, password
@@ -12,27 +14,11 @@ const register = async function ({
 	},
 }) {
 	try {
-		// Create a new instance of UserModel
-		const currentUser = new UserModel({
+		return createUser(UserModel, {
 			userName,
 			email,
 			password,
 		});
-
-		// Save the currentUser
-		currentUser
-			.save()
-			.then(() => currentUser)
-			.catch((error) => {
-				const errorMessage = 'Data Base error';
-				return boom.boomify(error, {
-					statusCode: 500,
-					message: errorMessage,
-				});
-			});
-
-		// Return the currentUser
-		return currentUser;
 	} catch (error) {
 		// Return if error
 		const errorMessage = 'API Failed';
