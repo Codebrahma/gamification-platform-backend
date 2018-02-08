5be74a669669c7373349fab6378d80771fa6d5ac
Arjun <snarjun92@gmail.com>
2018-02-08 20:19:37 +0530

    refactor(project): express connect to db and start server

--- server.js
+++ server.js
@@ -1,13 +1,78 @@
-const Hapi = require('hapi');
+const logger = require('./server/utils/logger');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const mongoose = require('mongoose');
 const config = require('config');
-const routes = require('./routes');
 
-const server = Hapi.Server({
-	port: config.get('app.port'),
+const isProduction = process.env.NODE_ENV === 'production';
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require('method-override')());
+
+if (!isProduction) {
+	app.use(errorhandler());
+}
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-// attach routes here
-server.route(routes);
+// error handlers
 
-// export modules
-module.exports = server;
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+	app.use((err, req, res) => {
+		logger.error(err.stack);
+
+		res.status(err.status || 500);
+
+		res.json({
+			errors: {
+				message: err.message,
+				error: err,
+			},
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+	res.status(err.status || 500);
+	res.json({
+		errors: {
+			message: err.message,
+			error: {},
+		},
+	});
+});
+
+function listen() {
+	if (app.get('env') === 'test') return;
+	app.listen(port);
+	logger.info(`Express app started on port ${port}`);
+}
+
+function connect() {
+	const options = { keepAlive: 1 };
+	mongoose.connect(config.get('app.dbConfig.url'), options);
+	return mongoose.connection;
+}
+
+connect()
+	.on('error', logger.error)
+	.on('disconnected', connect)
+	.once('open', listen);
