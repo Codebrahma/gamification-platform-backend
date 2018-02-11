96b11afcd7f03defd248190a0a345cfc71029753
Arjun <snarjun92@gmail.com>
2018-02-11 15:34:43 +0530

    refactor(user-registration): add validation for user registration

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -2,12 +2,21 @@ const User = require('../../db/schema/userSchema');
 
 exports.registration = function (req, res, next) {
 	const { userName, email, password } = req.body;
+	if (!userName || !email || !password) {
+		res.status(422).send({ error: 'Bad request. Request data invalid or missing.' });
+	}
 
-	const user = new User({
-		userName,
-		email,
-		password,
-	});
+	User.findOne({ $or: [{ email }, { userName }] })
+		.then((existingUser) => {
+			if (existingUser) {
+				return res.status(422).send({ error: 'That email address or username is already in use.' });
+			}
+			const user = new User({
+				userName,
+				email,
+				password,
+			});
 
-	user.save().then(newUser => res.json(newUser)).catch(next);
+			return user.save().then(newUser => res.json(newUser)).catch(next);
+		});
 };
