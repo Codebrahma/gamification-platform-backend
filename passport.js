3c6b9a0cb281db4dfdad8e794bc8c8afc5481f05
Arjun <snarjun92@gmail.com>
2018-02-11 18:46:32 +0530

    feat(user-login): add passport, token dependencies

---
+++ passport.js
@@ -0,0 +1,9 @@
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local');
+// const JwtStrategy = require('passport-jwt').Strategy;
+// const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// const localOptions = {
+//   usernameField: 'email'
+// };
+
