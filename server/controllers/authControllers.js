5c51f4dcd1f1b1400b08d84ea435b811aea0f68c
Arjun <snarjun92@gmail.com>
2018-02-11 19:20:55 +0530

    feat(user-registration): hash pw before saving to db

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt');
 const User = require('../../db/schema/userSchema');
+
+const saltRounds = 8;
 
 exports.registration = function (req, res, next) {
 	const { userName, email, password } = req.body;
@@ -9,14 +12,17 @@ exports.registration = function (req, res, next) {
 	User.findOne({ $or: [{ email }, { userName }] })
 		.then((existingUser) => {
 			if (existingUser) {
-				return res.status(422).send({ error: 'That email address or username is already in use.' });
+				res.status(422).send({ error: 'That email address or username is already in use.' });
+			} else {
+				bcrypt.hash(password, saltRounds)
+					.then((hash) => {
+						const user = new User({
+							userName,
+							email,
+							password: hash,
+						});
+						user.save().then(({ _id }) => res.json({ _id, userName, email })).catch(next);
+					});
 			}
-			const user = new User({
-				userName,
-				email,
-				password,
-			});
-
-			return user.save().then(newUser => res.json(newUser)).catch(next);
-		});
+		}).catch(next);
 };
